7027046ff74666da8bb12882634ad59395a0f978
Mikko Multimäki <mikko.multimaki@kolumbus.fi>
2025-09-22 21:27:10 +0300

    Fix test, but now we have a deprecation warning.

--- src/test/java/fi/tietotupsu/delena/kingmakerpf2/Kingmakerpf2ApplicationTests.java
+++ src/test/java/fi/tietotupsu/delena/kingmakerpf2/Kingmakerpf2ApplicationTests.java
@@ -1,10 +1,21 @@
 package fi.tietotupsu.delena.kingmakerpf2;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import fi.tietotupsu.delena.kingmakerpf2.repositories.PfCharacterRepository;
 
 @SpringBootTest
+@EnableAutoConfiguration(exclude = {
+    org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration.class,
+    org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration.class
+})
 class Kingmakerpf2ApplicationTests {
+	//TODO: Find a way to mock this properly, without deprecation warnings
+    @MockBean
+    private PfCharacterRepository pfCharacterRepository;
 
 	@Test
 	void contextLoads() {
